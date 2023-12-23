e408659ab5f2d889e47f7f17ca7a893f45c4f8be
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2023-12-23 18:00:05 -0500

    visual update

--- src/classes/character.ts
+++ src/classes/character.ts
@@ -2,11 +2,13 @@ export class Character {
   name: string;
   rarity: number;
   element: string;
+  path?: string;
   banner: string;
   splash: string;
 
   constructor(name, data) {
     this.name = name;
+    this.path = data.path;
     this.rarity = data.rarity;
     this.element = data.element;
     this.banner = data.banner;
