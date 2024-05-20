b63552079bb043d330bc95f62202ae88d772dadd
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-05-20 16:38:52 -0400

    refresh

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,41 +1,85 @@
-import { z, defineCollection, reference } from 'astro:content';
+import { z, defineCollection, reference } from "astro:content";
 
-const bannerCollection = defineCollection({
-    type: "data",
-    schema: z.object({
-        time: z.string(),
-        characters: z.object({
-            five: z.array(z.string()),
-            four: z.array(z.string())
-        }),
-        weapons: z.array(z.string())
-    }).partial()
+const gameCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    icon: z.string(),
+    maintenance: z.string().time(),
+    update: z.string().time(),
+    reset: z.string().time(),
+  }),
 });
 
-const versionsCollection = defineCollection({
-    type: "data",
-    schema: z.object({
-        version: z.number(),
-        name: z.string(),
-        chronicle: z.object({
-            characters: z.array(z.string()),
-            weapons: z.array(z.string())
-        })
-    }).partial()
-})
+const characterCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    rarity: z.number(),
+    element: z.string(),
+    weaponType: z.string().optional(),
+    bannerName: z.string().optional(),
+    splashArt: z.string().optional(),
+  }),
+});
 
-const issuesCollection = defineCollection({
-    type:"content",
-    schema: z.object({
-        title: z.string(),
-        image: z.string(),
-        publishedDate: z.string(),
-        tags: z.array(z.string())
-    })
-})
+const weaponsCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    rarity: z.number(),
+    weaponType: z.string(),
+    splashImage: z.string(),
+  }),
+});
+
+const eventCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    type: z.string(),
+    game: reference("games"),
+    startDate: z.string().date(),
+  }),
+});
+
+// const bannerCollection = defineCollection({
+//     type: "data",
+//     schema: z.object({
+//         time: z.string(),
+//         characters: z.object({
+//             five: z.array(z.string()),
+//             four: z.array(z.string())
+//         }),
+//         weapons: z.array(z.string())
+//     }).partial()
+// });
+
+// const versionsCollection = defineCollection({
+//     type: "data",
+//     schema: z.object({
+//         version: z.number(),
+//         name: z.string(),
+//         chronicle: z.object({
+//             characters: z.array(z.string()),
+//             weapons: z.array(z.string())
+//         })
+//     }).partial()
+// })
+
+// const issuesCollection = defineCollection({
+//     type:"content",
+//     schema: z.object({
+//         title: z.string(),
+//         image: z.string(),
+//         publishedDate: z.string(),
+//         tags: z.array(z.string())
+//     })
+// })
 
 export const collections = {
-  'banners': bannerCollection,
-  'versions': versionsCollection,
-  'issues': issuesCollection
+  games: gameCollection,
+  characters: characterCollection,
+  weapons: weaponsCollection,
+  events: eventCollection,
 };
