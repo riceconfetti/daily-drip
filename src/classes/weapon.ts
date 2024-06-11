549434491fa270c9dda38e1074536223d28e9bef
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-06-11 19:53:20 -0400

    classes and calendar

--- src/classes/weapon.ts
+++ src/classes/weapon.ts
@@ -1,17 +1,8 @@
-export class Weapon {
-    name: string;
-    rarity: number;
-    element?: string;
-    type?: string;
-    path?: string;
-    splash: string;
-  
-    constructor(name, data) {
-      this.name = name;
-      this.path = data.path;
-      this.type = data.type;
-      this.element = data.element;
-      this.rarity = data.rarity;
-      this.splash = data.splash;
-    }
-  }
+export type Weapon = {
+  name: string;
+  rarity: number;
+  element?: string;
+  type?: string;
+  path?: string;
+  splash: string;
+};
