1abe6ac4c80fb482da2e38aee9879d0163b00895
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-06-11 15:57:54 -0400

    changes

--- src/components/EventsWidget.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  let currentEvents;
-  let upcomingEvents;
-</script>
-
-<section>
-  <h2 class="playfair-display-regular">Current Events</h2>
-  <h2 class="playfair-display-regular">Upcoming Events</h2>
-</section>
