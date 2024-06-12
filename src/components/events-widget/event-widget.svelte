ad445d71e7d25664b05e03d851892db16cd05110
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-06-12 13:08:03 -0400

    homepage events hydrated

---
+++ src/components/events-widget/event-widget.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import EventSingle from "./event-single.svelte";
+  export let events;
+
+  $: currentEvents = events.filter((e) => {
+    console.log(e.startDate);
+    return dayjs(e.startDate) < dayjs() && dayjs(e.endDate) > dayjs();
+  });
+
+  $: upcomingEvents = events.filter((e) => {
+    return (
+      dayjs(e.startDate) > dayjs() && dayjs(e.startDate) < dayjs().add(4, "w")
+    );
+  });
+</script>
+
+<section class="flex flex-col gap-2 w-full">
+  <h2 class="playfair-display-semibold text-dark">Current Events</h2>
+  {#each currentEvents as eventDetails}
+    <EventSingle {eventDetails} />
+  {/each}
+  <h2 class="playfair-display-semibold text-dark">Upcoming Events</h2>
+  {#each upcomingEvents as eventDetails}
+    <EventSingle {eventDetails} />
+  {/each}
+</section>
