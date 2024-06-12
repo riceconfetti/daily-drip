024043429a4ee20d5bd258d9acc40f289757b5eb
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-06-11 21:13:20 -0400

    fixed events

--- src/classes/character.ts
+++ src/classes/character.ts
@@ -1,17 +1,17 @@
 export class Character {
-  name: string;
-  rarity: number;
-  element: string;
+  name?: string;
+  rarity?: number;
+  element?: string;
   weaponType?: string;
   bannerName?: string;
   colors?: {
-    primary: string;
-    secondary: string;
-    textAccent: string;
+    primary?: string;
+    secondary?: string;
+    textAccent?: string;
   };
   images?: {
-    gachaSplash: string;
-    gachaCard: string;
-    bannerCard: string;
+    gachaSplash?: string;
+    gachaCard?: string;
+    bannerCard?: string;
   };
 }
