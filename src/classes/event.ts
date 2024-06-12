9337905cbec485a85ac4dd1f6af51a2e1de392f5
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-06-12 13:08:03 -0400

    homepage events hydrated

--- src/classes/event.ts
+++ src/classes/event.ts
@@ -1,17 +1,25 @@
-import type { Game } from "./game";
-import type { Character } from "./character";
 export class Event {
-  status?: string;
-  type?: string;
+  status: string;
+  type: string;
+  game: string;
+  startDate: string;
+  endDate: string;
+  title: string;
+  image: string;
   colors?: {
     primary?: string;
     secondary?: string;
     textAccent?: string;
   };
-  game?: Game;
-  startDate?: string;
-  endDate?: string;
-  title?: string;
-  character?: Character;
-  image?: string;
+
+  constructor(data) {
+    this.status = data.status;
+    this.type = data.type;
+    this.game = data.game.id;
+    this.startDate = data.start;
+    this.endDate = data.end;
+    this.title = data.title;
+    this.image = data.image;
+    this.colors = data.colors;
+  }
 }
---
+++ src/components/events-widget/event-single.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Image } from "@unpic/svelte";
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime.js";
+  dayjs.extend(relativeTime);
+  export let eventDetails;
+
+  const eventType =
+    dayjs(eventDetails.startDate) > dayjs() ? "Starting" : "Ending";
+  $: timeUntil =
+    dayjs(eventDetails.startDate) > dayjs()
+      ? dayjs().to(dayjs(eventDetails.startDate))
+      : dayjs().to(dayjs(eventDetails.endDate));
+
+  const fonts = {
+    genshin: "hy-impact-regular",
+    starrail: "bai-jamjuree-bold",
+    reverse: "playfair-display-sc-bold",
+    wuwa: "philosopher-bold",
+  };
+</script>
+
+<div
+  class={`w-full relative p-4 flex flex-col gap-2 rounded-md overflow-hidden ${eventDetails.colors.primary} ${fonts[eventDetails.game]}`}
+>
+  <h2 class="text-white text-sm z-10">{eventDetails.title}</h2>
+  <h3 class={`${eventDetails.colors.textAccent} text-xs z-10`}>
+    {eventType}
+    {timeUntil}
+  </h3>
+  <Image
+    src={eventDetails.image}
+    height={1200}
+    width={4600}
+    class="absolute right-0 top-0 h-full w-auto gradient-mask-l-[transparent,rgba(0,0,0,1.0)_0px,rgba(0,0,0,0.8)_30%]"
+    alt={`${eventDetails.title} Banner Card`}
+  />
+</div>
