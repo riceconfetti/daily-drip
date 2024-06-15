4b13882683888af379bef4f6941c61dbaebea548
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-06-15 16:37:19 -0400

    optimizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import { enhancedImages } from "@sveltejs/enhanced-img";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import tailwind from "@astrojs/tailwind";
 import swup from "@swup/astro";
 import svelte from "@astrojs/svelte";
@@ -10,6 +11,7 @@ export default defineConfig({
     enhancedImages(),
     tailwind(),
     svelte(),
+    ViteImageOptimizer({}),
     swup({ theme: ["overlay", { direction: "to-bottom" }] }),
   ],
 });
