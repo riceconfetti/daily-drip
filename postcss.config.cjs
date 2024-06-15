2b4b841517b26d87556ace326b9d61b407d5c682
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-06-15 19:19:44 -0400

    removed swup and added generic loader

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
 // postcss.config.js
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      require('tailwindcss'),
-      require('postcss-import')
-    ]|
-  }
+  plugins: [
+    require("autoprefixer"),
+    require("tailwindcss"),
+    require("postcss-import"),
+  ],
+};
---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import "ldrs/square";
+
+export default function Loader() {
+  return (
+      <l-square
+        size="35"
+        stroke="5"
+        stroke-length="0.25"
+        bg-opacity="0.1"
+        speed="1.2"
+        color="rgb(193 156 100)"
+      ></l-square>
+  );
+}
