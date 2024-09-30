3abf6ca9d64af54faa3582a877cd740ef656a02b
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-09-30 08:41:13 -0400

    calendar performance fix

--- src/components/CalendarWidget.svelte
+++ src/components/CalendarWidget.svelte
@@ -10,133 +10,61 @@
 
 	export let events, versions, games
 
-	function findGame(game, date) {
-		// console.log(dayjs(date))
-		return game.find((e) => e.isSame(dayjs(date), 'day'))
+	let eventDates = Object.fromEntries(
+		events.map((e) => {
+			let game = e.data.game.id
+			let version = versions.find((v) => v.id.startsWith(game + '/' + e.id.split('/')[1])).data
+			let gameData = games.find((g) => g.id.startsWith(game)).data
+
+			let event = []
+			if (e.data.startDate == version.startDate) {
+				event.push(
+					dayjs
+						.utc(e.data.startDate + `T${gameData.times.version}Z`)
+						.tz(dayjs.tz.guess())
+						.format('YYYY-MM-DD')
+				)
+			} else {
+				event.push(
+					dayjs
+						.utc(
+							e.data.startDate +
+								`T${gameData.times.update.find((t) => t.zone == settings.get().genshin).time}Z`
+						)
+						.tz(dayjs.tz.guess())
+						.format('YYYY-MM-DD')
+				)
+			}
+			event.push(game)
+			return event
+		})
+	)
+
+	function getEvent(date) {
+		return eventDates[dayjs(date).format()]
 	}
 
-	let eventDates = {
-		genshin: events.filter(({ id }) => id.startsWith('genshin/')),
-		starrail: events.filter(({ id }) => id.startsWith('starrail/')),
-		wuwa: events.filter(({ id }) => id.startsWith('wuwa/')),
-		zzz: events.filter(({ id }) => id.startsWith('zzz/'))
-	}
-	// console.log(eventDates)
-
-	const genshin = [
-		...new Set(
-			eventDates.genshin.map((e) => {
-				let version = versions.find((v) => v.id.startsWith('genshin/' + e.id.split('/')[1])).data
-				let game = games.find((g) => g.id.startsWith('genshin')).data
-
-				if (e.data.startDate == version.startDate) {
-					return dayjs.utc(e.data.startDate + `T${game.times.version}Z`).tz(dayjs.tz.guess())
-				} else {
-					return dayjs
-						.utc(
-							e.data.startDate +
-								`T${game.times.update.find((t) => t.zone == settings.get().genshin).time}Z`
-						)
-						.tz(dayjs.tz.guess())
-				}
-			})
-		)
-	]
-
-	// console.data.log(genshin)
-
-	const starrail = [
-		...new Set(
-			eventDates.starrail.map((e) => {
-				let version = versions.find((v) => v.id.startsWith('starrail/' + e.id.split('/')[1])).data
-				let game = games.find((g) => g.id.startsWith('starrail')).data
-
-				if (e.data.startDate == version.startDate) {
-					return dayjs.utc(e.data.startDate + `T${game.times.version}Z`).tz(dayjs.tz.guess())
-				} else {
-					return dayjs
-						.utc(
-							e.data.startDate +
-								`T${game.times.update.find((t) => t.zone == settings.get().starrail).time}Z`
-						)
-						.tz(dayjs.tz.guess())
-				}
-			})
-		)
-	]
-
-	const wuwa = [
-		...new Set(
-			eventDates.wuwa.map((e) => {
-				let version = versions.find((v) => v.id.startsWith('wuwa/' + e.id.split('/')[1])).data
-				let game = games.find((g) => g.id.startsWith('wuwa')).data
-
-				if (e.data.startDate == version.startDate) {
-					return dayjs.utc(e.data.startDate + `T${game.times.version}Z`).tz(dayjs.tz.guess())
-				} else {
-					return dayjs
-						.utc(
-							e.data.startDate +
-								`T${game.times.update.find((t) => t.zone == settings.get().wuwa).time}Z`
-						)
-						.tz(dayjs.tz.guess())
-				}
-			})
-		)
-	]
-
-	const zzz = [
-		...new Set(
-			eventDates.zzz.map((e) => {
-				let version = versions.find((v) => v.id.startsWith('zzz/' + e.id.split('/')[1])).data
-				let game = games.find((g) => g.id.startsWith('zzz')).data
-
-				if (e.data.startDate == version.startDate) {
-					return dayjs.utc(e.data.startDate + `T${game.times.version}Z`).tz(dayjs.tz.guess())
-				} else {
-					return dayjs
-						.utc(
-							e.data.startDate +
-								`T${game.times.update.find((t) => t.zone == settings.get().zzz).time}Z`
-						)
-						.tz(dayjs.tz.guess())
-				}
-			})
-		)
-	]
-
-	function isDate(date) {
-		let game = ''
-		if (findGame(genshin, date)) {
-			game = 'genshin'
-		} else if (findGame(starrail, date)) {
-			game = 'starrail'
-		} else if (findGame(wuwa, date)) {
-			game = 'wuwa'
-		} else if (findGame(zzz, date)) {
-			game = 'zzz'
-		}
-		return game
+	const isDateUnavailable: Calendar.Props['isDateUnavailable'] = (date) => {
+		return dayjs(date).format('YYYY-MM-DD') in eventDates
 	}
 </script>
 
 <Calendar.Root
 	let:months
 	let:weekdays
+	{isDateUnavailable}
 	class="flex w-full flex-col p-4 text-center md:grid md:h-full md:grid-rows-[fit-content(theme(spacing.32))] md:min-h-0"
 	fixedWeeks={true}
 	weekdayFormat="short"
 >
 	<Calendar.Header class="playfair-display-sc-bold flex items-center justify-between gap-2 p-2">
 		<Calendar.PrevButton
-			disabled
 			class="rounded-9px bg-background-alt hover:bg-muted active:scale-98 inline-flex size-10 items-center justify-center active:transition-all"
 		>
 			<i class="ri-arrow-left-s-line"></i>
 		</Calendar.PrevButton>
 		<Calendar.Heading />
 		<Calendar.NextButton
-			disabled
 			class="rounded-9px bg-background-alt hover:bg-muted active:scale-98 inline-flex size-10 items-center justify-center active:transition-all"
 		>
 			<i class="ri-arrow-right-s-line"></i>
@@ -170,19 +98,24 @@
 								{date}
 								class="min-h-0 p-2 md:aspect-square md:h-full md:w-full md:border-black md:border-opacity-20 md:p-1 md:px-2"
 							>
+								{@const event = isDateUnavailable
+									? eventDates[dayjs(date).format('YYYY-MM-DD')]
+									: ''}
 								<Calendar.Day
 									{date}
 									month={month.value}
-									data-game={isDate(date)}
+									data-game={event}
 									class="crimson-text-regular rounded-9px text-foreground hover:border-foreground data-[selected]:text-background data-[unavailable]:text-muted-foreground group relative inline-flex aspect-square h-auto w-full items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-transparent p-2 text-sm font-normal leading-none data-[outside-month]:pointer-events-none data-[game='genshin']:bg-genshin-event data-[game='zzz']:bg-zzz-event data-[game='starrail']:bg-starrail-event data-[game='wuwa']:bg-wuwa-event data-[game='genshin']:text-white data-[game='zzz']:text-white data-[game='starrail']:text-white data-[game='wuwa']:text-white data-[disabled]:opacity-20 md:min-h-0 md:w-min md:p-[.35rem]"
 								>
 									{date.day}
 								</Calendar.Day>
-								<p
-									class={`ml-1 hidden w-min ${isDate(date) == 'genshin' ? 'text-2xs' : 'text-xs'} ${isDate(date)} text-[#535353] md:inline xl:${isDate(date) == 'genshin' ? 'text-xs' : 'text-sm'}`}
-								>
-									{isDate(date).charAt(0).toUpperCase() + isDate(date).slice(1)}
-								</p>
+								{#if event}
+									<p
+										class={`ml-1 hidden w-min ${event == 'genshin' ? 'text-2xs' : 'text-xs'} ${event} text-[#535353] md:inline xl:${event == 'genshin' ? 'text-xs' : 'text-sm'}`}
+									>
+										{event.charAt(0).toUpperCase() + event.slice(1)}
+									</p>
+								{/if}
 							</Calendar.Cell>
 						{/each}
 					</Calendar.GridRow>
