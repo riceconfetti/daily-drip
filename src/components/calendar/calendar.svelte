ed67448adfa42adc573d420faa38815eeaa947a3
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-10-10 19:55:50 -0400

    continuous scroll!

--- src/components/calendar/calendar.svelte
+++ src/components/calendar/calendar.svelte
@@ -37,60 +37,134 @@
 		.map((_, i) => dayjs([year, month, i + 1]))
 		.groupBy((d) => d.isoWeek())
 
-	// console.log(calendarMap)
-	let calendar = Object.entries(calendarMap).map((x) => x[1])
+	function initCalendar(calendarMap) {
+		let calendar = {
+			before: [],
+			main: Object.entries(calendarMap).map((x) => x[1]),
+			after: []
+		}
 
-	for (const week of calendar) {
-		if (week[0].date() == 1) {
-			while (week.length < 7) {
-				week.unshift(week[0].subtract(1, 'day'))
+		for (const week of calendar.main) {
+			if (week[0].date() == 1) {
+				while (week.length < 7) {
+					week.unshift(week[0].subtract(1, 'day'))
+				}
+			}
+
+			if (week.at(-1).date() == week.at(-1).daysInMonth()) {
+				while (week.length < 7) {
+					week.push(week.at(-1).add(1, 'day'))
+					// console.log(week)
+				}
 			}
 		}
 
-		if (week.at(-1).date() == week.at(-1).daysInMonth()) {
-			while (week.length < 7) {
-				week.push(week.at(-1).add(1, 'day'))
-				// console.log(week)
+		while (calendar.main.length > 4) {
+			calendar.after = calendar.main.pop()
+		}
+
+		if (calendar.before.length == 0) {
+			calendar.before.unshift(calendar.main[0][0].subtract(1, 'day'))
+			while (calendar.before.length < 7) {
+				calendar.before.unshift(calendar.before[0].subtract(1, 'day'))
 			}
 		}
-	}
 
-	let weekBefore = []
-	let weekAfter = []
-	while (calendar.length > 4) {
-		weekAfter = calendar.pop()
-	}
-
-	if (weekBefore.length == 0) {
-		weekBefore.unshift(calendar[0][0].subtract(1, 'day'))
-		while (weekBefore.length < 7) {
-			weekBefore.unshift(weekBefore[0].subtract(1, 'day'))
+		if (calendar.after.length == 0) {
+			calendar.after.push(calendar.main.at(-1).at(-1).add(1, 'day'))
+			while (calendar.after.length < 7) {
+				calendar.after.push(calendar.after.at(-1).add(1, 'day'))
+			}
 		}
+
+		return calendar
 	}
 
-	if (weekAfter.length == 0) {
-		weekAfter.push(calendar.at(-1).at(-1).add(1, 'day'))
-		while (weekBefore.length < 7) {
-			weekAfter.push(weekAfter.at(-1).add(1, 'day'))
+	function goBackWeek(calendar) {
+		let temp = calendar
+		temp.main.unshift(temp.before)
+		temp.after = temp.main.pop()
+		temp.before = []
+
+		let eDate = temp.main[0][0]
+		for (let i = 0; i < 7; ++i) {
+			temp.before.unshift(eDate.subtract(1, 'day'))
+			eDate = temp.before[0]
 		}
+		return temp
 	}
 
-	$: refDate = calendar[2][0]
+	function goForwardWeek(calendar) {
+		let temp = calendar
+		temp.main.push(temp.after)
+		temp.before = temp.main.shift()
+		temp.after = []
+
+		let eDate = temp.main.at(-1).at(-1)
+		for (let i = 0; i < 7; ++i) {
+			temp.after.push(eDate.add(1, 'day'))
+			eDate = temp.after.at(-1)
+		}
+		return temp
+	}
+
+	let calendar = initCalendar(calendarMap)
+	let newCalendar = initCalendar(calendarMap)
+	let refDate = calendar.main[1][4]
 
 	onMount(() => {
 		gsap.registerPlugin(ScrollTrigger, Observer)
-
 		gsap.set('.weekBefore', { yPercent: -100 })
-		function getbefore() {
-			gsap.to('.weekBefore', { yPercent: 0 })
-			gsap.to('.mainWeeks', { yPercent: 25 })
+		gsap.set('.weekAfter', { yPercent: 100 })
+
+		let animating
+		ScrollTrigger.observe({
+			trigger: '.calendar',
+			type: 'wheel,touch',
+			onUp: () => !animating && getAfter(),
+			onDown: () => !animating && getBefore(),
+			wheelSpeed: -1,
+			tolerance: 50,
+			preventDefault: true,
+			onPress: (self) => {
+				// on touch devices like iOS, if we want to prevent scrolling, we must call preventDefault() on the touchstart (Observer doesn't do that because that would also prevent side-scrolling which is undesirable in most cases)
+				ScrollTrigger.isTouch && self.event.preventDefault()
+			}
+		})
+
+		function getBefore() {
+			animating = true
+			gsap.to('.calendar .weekBefore', { yPercent: 0 })
+			gsap.to('.calendar .mainWeeks', {
+				yPercent: 25,
+				onComplete: () => {
+					calendar = goBackWeek(calendar)
+					refDate = calendar.main[1][4]
+					gsap.set('.calendar .weekBefore', { yPercent: -100 })
+					gsap.set('.calendar .mainWeeks', { yPercent: 0 })
+					animating = false
+				}
+			})
 		}
 
-		document.querySelector('.mainWeeks').addEventListener('click', getbefore)
+		function getAfter() {
+			animating = true
+			gsap.to('.calendar .weekAfter', { yPercent: 0 })
+			gsap.to('.calendar .mainWeeks', {
+				yPercent: -25,
+				onComplete: () => {
+					calendar = goForwardWeek(calendar)
+					refDate = calendar.main[1][4]
+					gsap.set('.calendar .weekAfter', { yPercent: 100 })
+					gsap.set('.calendar .mainWeeks', { yPercent: 0 })
+					animating = false
+				}
+			})
+		}
 	})
 </script>
 
-<div class="w-full h-full p-2 flex flex-col gap-2">
+<main id="fullPageCalendar" class="w-full h-full p-2 flex flex-col gap-2">
 	<h2 class="playfair-display-sc-bold text-xl text-black text-opacity-70">
 		{refDate.format('MMMM')}
 	</h2>
@@ -106,9 +180,9 @@
 		<div
 			class="panel weekBefore z-10 top-0 border-b-0 absolute inset-x-0 border box-border border-dark border-opacity-30 text-sm divide-x divide-dark divide-opacity-30 grid grid-cols-7 bg-light h-1/4"
 		>
-			{#each weekBefore as day}
+			{#each calendar.before as day}
 				<div
-					class={`${columns[day.day()]} p-2 crimson-text-regular ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
+					class={`${columns[day.day()]} p-2 crimson-text-regular ${dayjs().isSame(day, 'day') ? 'text-accent-light' : ''} ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
 				>
 					{day.date()}
 				</div>
@@ -119,13 +193,13 @@
 		<div
 			class="panel mainWeeks box-border absolute inset-0 border border-dark border-opacity-30 w-full grid grid-cols-7 auto-rows-auto divide-y divide-dark divide-opacity-30 bg-light"
 		>
-			{#each calendar as week, index}
+			{#each calendar.main as week, index}
 				<div
 					class=" text-sm divide-x divide-dark divide-opacity-30 grid grid-cols-subgrid w-full col-span-7"
 				>
 					{#each week as day}
 						<div
-							class={`${columns[day.day()]} p-2 crimson-text-regular ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
+							class={`${columns[day.day()]} p-2 crimson-text-regular ${dayjs().isSame(day, 'day') ? 'text-accent-light' : ''} ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
 						>
 							{day.date()}
 						</div>
@@ -138,13 +212,13 @@
 		<div
 			class="panel box-border border-t-0 weekAfter -z-10 bottom-0 absolute inset-x-0 border border-dark border-opacity-30 text-sm divide-x divide-dark divide-opacity-30 grid grid-cols-7 bg-light h-1/4"
 		>
-			{#each weekAfter as day}
+			{#each calendar.after as day}
 				<div
-					class={`${columns[day.day()]} p-2 crimson-text-regular  ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
+					class={`${columns[day.day()]} p-2 crimson-text-regular ${dayjs().isSame(day, 'day') ? 'text-accent-light' : ''} ${day.month() != refDate.month() ? 'bg-dark bg-opacity-10' : ''}`}
 				>
 					{day.date()}
 				</div>
 			{/each}
 		</div>
 	</div>
-</div>
+</main>
