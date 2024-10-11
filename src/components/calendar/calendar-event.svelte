97200287f712dc0462d1ef5f7daea3c802622a67
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-10-10 20:30:20 -0400

    event to button

--- src/components/calendar/calendar-event.svelte
+++ src/components/calendar/calendar-event.svelte
@@ -2,4 +2,6 @@
 	export let game, event
 </script>
 
-<p class={`mx-2 px-2 col-span-3 rounded col-start-1 h-5 overflow-hidden ${game} `}>Burnice</p>
+<button class={`mx-2 px-2 col-span-3 rounded col-start-1 h-5 overflow-hidden ${game} `}
+	>Burnice</button
+>
