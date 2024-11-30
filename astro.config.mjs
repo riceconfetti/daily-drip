4899fb79f0716e4aede943dae6bea15d2220755c
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-11-30 11:50:47 -0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 // @ts-check
 // @ts-check
 import { defineConfig, envField} from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 import dotenv from 'dotenv'
 dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()], 
+  integrations: [svelte()], 
   env: {
     schema: {
       IMGPROXY_KEY: envField.string({ context: "server", access: "secret" }),
       IMGPROXY_SALT: envField.string({ context: "server", access: "secret" }),
     }
+  },
+  vite: {
+    plugins: [ tailwindcss()]
   }
 });
