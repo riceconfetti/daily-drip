fbfd3eda2f48e19b572e951e89e8f4f51b892700
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2024-11-30 11:20:29 -0500

    beta rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	prefetch: {
-		defaultStrategy: 'viewport',
-		prefetchAll: true
-	},
-	integrations: [tailwind(), svelte()]
-})
+  integrations: [tailwind(), svelte()]
+});
--- src/classes/game.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-const Game = z.object({
-	name: z.string(),
-	icon: z.string(),
-	times: z.object({})
-})
-
-export type Game = z.infer<typeof Game>
