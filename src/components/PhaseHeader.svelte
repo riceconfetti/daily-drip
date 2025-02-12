47c41d239bbd0c8fc361aff0ab5a1d59a7376642
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-02-12 16:00:17 -0500

    landscape update

--- src/components/PhaseHeader.svelte
+++ src/components/PhaseHeader.svelte
@@ -5,24 +5,26 @@
   dayjs.extend(utc);
   dayjs.extend(timezone);
 
-  let { game = "genshin", startDate, phase } = $props();
+  let { game = "genshin", event } = $props();
 
   import { getServer } from "$store/settings.svelte";
   const server = $derived(getServer(game));
   let currzone = dayjs.tz.guess();
-  const tz = $derived(phase.phase == "1" ? "+08:00" : server?.value?.timezone);
+  const tz = $derived(
+    event.phase.phase == "1" ? "+08:00" : server?.value?.timezone,
+  );
 </script>
 
 <div
-  class={`tracking-normal h-min font-semibold font-subheading flex w-full justify-between border border-black/10 p-2 text-xs sm:text-sm ${phase.phase == "1" ? "bg-[#BFBFBF] text-text-light" : "bg-dark text-accent-gold"}`}
+  class={`tracking-normal h-min font-semibold font-subheading flex w-full justify-between border border-black/10 p-2 text-xs sm:text-sm ${event.phase.phase == "1" ? "bg-[#BFBFBF] text-text-light" : "bg-dark text-accent-gold order-0"}`}
 >
-  <h1>Phase {phase.phase}</h1>
+  <h1>Phase {event.phase.phase}</h1>
   <p class="font-light italic">
-    {dayjs(startDate + "T" + phase.start + tz)
+    {dayjs(event.startDate + "T" + event.phase.start + tz)
       .tz(currzone)
       .format("MMM D") +
       " @" +
-      dayjs(startDate + "T" + phase.start + tz)
+      dayjs(event.startDate + "T" + event.phase.start + tz)
         .tz(currzone)
         .format("h a")}
   </p>
